426da43af43df71cb5c33f661087483c604fa215
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-11-04 15:19:07 +0700

    04/10

---
+++ src/main/java/hieu/springbootcustomer/SpringbootCustomerApplication.java
@@ -0,0 +1,13 @@
+package hieu.springbootcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCustomerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hieu/springbootcustomer/SpringbootCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.springbootcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
